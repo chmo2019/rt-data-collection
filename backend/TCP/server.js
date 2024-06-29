960eab4852cd7bf838f6214687396892da28319a
charles <mo.charles2019@gmail.com>
2024-06-28 22:52:46 -0400

    add start/stop events

--- backend/TCP/server.js
+++ backend/TCP/server.js
@@ -6,7 +6,7 @@ const http = require('http');
 
 // third party
 const express = require('express');
-const { Server, Socket } = require("socket.io");
+const { Server } = require("socket.io");
 
 // load env variables
 require('dotenv').config();
@@ -19,6 +19,9 @@ const tcp = net.createServer();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+// array of socket
+let sockets = [];
 
 function tryParseJson(str) {
     try {
@@ -34,7 +37,7 @@ function getData(payload) {
     const sock = this;
 
     // print data to console
-    console.log('DATA ' + sock.remoteAddress + ': ' + payload);
+    // console.log('DATA ' + sock.remoteAddress + ': ' + payload);
 
     // try parsing json payload
     const parsedData = tryParseJson(payload);
@@ -43,7 +46,7 @@ function getData(payload) {
     if (parsedData) {
         fs.appendFile(`${process.env.DATA_PATH}${parsedData.name}.txt`, `${parsedData.ts},${parsedData.data}\n`, (err) => {
             if (err) return console.log(err);
-            console.log(parsedData);
+            // console.log(parsedData);
         })
     }
 
@@ -51,28 +54,61 @@ function getData(payload) {
     sock.write(sock.remoteAddress + ':' + sock.remotePort + " said " + payload + '\n');
 }
 
-function sendData(payload) {
-    // get socket
-    const sock = this;
-
-    sock.emit('data', payload);
-}
-
 function sendDataFromDisk(fname) {
+    // get socker
+    sock = this;
+
     // read file data into memory
     const filedata = fs.readFileSync(fname, 'utf8').toString().split('\n');
 
     // send data
-    sendData(filedata[filedata.length-2]);
+    sock.emit('data', filedata);
 }
 
-// listen to connection events: https://www.digitalocean.com/community/tutorials/how-to-develop-a-node-js-tcp-server-application-using-pm2-and-nginx-on-ubuntu-16-04
-tcp.on('connection', (sock) => { 
-    sock.on('data', getData);
+function onClose() {
+    sock = this;
+
+    let index = sockets.findIndex(function(o) {
+        return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
+    })
+    if (index !== -1) sockets.splice(index, 1);
+    // console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+}
+
+function onDisconnect() {
+    console.log('user disconnected');
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/test.html');
 });
 
+// listen to connection events: https://www.digitalocean.com/community/tutorials/how-to-develop-a-node-js-tcp-server-application-using-pm2-and-nginx-on-ubuntu-16-04
+tcp.on('connection', (sock) => {
+    sockets.push(sock); 
+    sock.on('data', getData);
+    sock.on('close', onClose);
+    // io.emit('data', 'hello');
+});
+
+// socket io connection handler
 io.on('connection', (socket) => {
-    io.on("data", sendDataFromDisk);
+    console.log('user connected');
+
+    let intervalId;
+
+    socket.on('start', (data) => {
+        console.log(data);
+        if (data === "start") {
+            intervalId = setInterval(() => {io.emit('data', 'hello')}, 2000);
+        } else if (data === "stop") {
+            clearInterval(intervalId);
+        }
+    });
+
+    // sendDataFromDisk(io, `${process.env.DATA_PATH}test-client.txt`);
+    // socket.on("data", sendDataFromDisk, `${process.env.DATA_PATH}test-client.txt`);
+    socket.on('disconnect', onDisconnect);
 });
 
 // start tcp server
